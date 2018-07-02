db6e9d03af86f785c6dec7f3a70f5abcfbdafc5d
Jep Alaba <jep@jep-mbp.local>
2018-07-01 17:43:51 -0700

    Added animations on first level

---
+++ src/js/animator.js
@@ -0,0 +1,110 @@
+import autobind from 'autobind-decorator'
+
+const fastTransition = '-webkit-transform 0.2s cubic-bezier(0, 0.75, 0.25, 1)'
+const slowTransition = '-webkit-transform 0.4s cubic-bezier(0.2, 0.8, 0.4, 1)'
+
+export default class Animator {
+	static timeouts = {
+		protosLobbies: 400,
+		lobbyCard: 200
+	}
+
+	constructor(page) {
+		this.page = page
+	}
+
+	@autobind protosLobbiesExit(node) {
+		this.protosLobbiesLeft = this.protosLobbiesLeft || node.offsetLeft
+
+		const pageStyle = this.page.current.style
+		const nodeStyle = node.style
+		const oldLobbyCardLeft = this.lobbyCardLeft
+
+		window.requestAnimationFrame(() => {
+			nodeStyle.position = 'absolute'
+
+			this.lobbySettingsOffset = oldLobbyCardLeft - this.lobbyCard.offsetLeft
+
+			nodeStyle.left = `${this.protosLobbiesLeft - this.lobbySettingsOffset}px`
+
+			pageStyle.webkitTransition = ''
+			pageStyle.webkitTransform = `translate3d(${this.lobbySettingsOffset}px, 0, 0)`
+
+			window.requestAnimationFrame(() => {
+				pageStyle.webkitTransition = slowTransition
+				pageStyle.webkitTransform = ''
+			})
+		})
+	}
+
+	@autobind lobbyCardEnter(node) {
+		this.lobbyCard = node
+		this.lobbyCardLeft = node.offsetLeft
+		this.lobbyCardOffset = node.offsetWidth/2
+
+		const pageStyle = this.page.current.style
+
+		window.requestAnimationFrame(() => {
+			pageStyle.webkitTransition = ''
+			pageStyle.webkitTransform = `translate3d(${this.lobbyCardOffset}px, 0, 0)`
+
+			window.requestAnimationFrame(() => {
+				pageStyle.webkitTransition = fastTransition
+				pageStyle.transform = ''
+			})
+		})
+	}
+
+	@autobind lobbyCardExit(node) {
+		const pageStyle = this.page.current.style
+		const nodeStyle = node.style
+
+		window.requestAnimationFrame(() => {
+			pageStyle.webkitTransition = ''
+			pageStyle.transform = `translate3d(-${this.lobbyCardOffset}px, 0, 0)`
+
+			nodeStyle.position = 'absolute'
+			nodeStyle.left = `${this.lobbyCardLeft + this.lobbyCardOffset}px`
+
+			window.requestAnimationFrame(() => {
+				pageStyle.webkitTransition = fastTransition
+				pageStyle.transform = ''
+			})
+		})
+	}
+
+	@autobind lobbyCardSwitch(node) {
+		const style = node.style
+		const computedStyle = window.getComputedStyle(node)
+		const left = `${node.offsetLeft - node.offsetWidth/2 - parseInt(computedStyle.marginLeft) - parseInt(computedStyle.marginRight)}px`
+
+		window.requestAnimationFrame(() => {
+			style.position = 'absolute'
+			style.left = left
+		})
+	}
+
+	@autobind lobbySettingsEnter(node) {
+		this.lobbySettingsLeft = node.offsetLeft
+		this.lobbySettingsWidth = node.offsetWidth
+	}
+
+	@autobind lobbySettingsExit(node) {
+		const pageStyle = this.page.current.style
+		const nodeStyle = node.style
+
+		window.requestAnimationFrame(() => {
+			pageStyle.webkitTransition = ''
+			pageStyle.webkitTransform = `translate3d(-${this.lobbySettingsOffset}px, 0, 0)`
+
+			nodeStyle.position = 'absolute'
+			nodeStyle.left = `${this.lobbySettingsLeft + this.lobbySettingsOffset + this.lobbyCardOffset}px`
+			nodeStyle.width = `${this.lobbySettingsWidth}px`
+
+			window.requestAnimationFrame(() => {
+				pageStyle.webkitTransition = slowTransition
+				pageStyle.webkitTransform = ''
+			})
+		})
+	}
+}
--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,16 +1,28 @@
-import Dev from 'mobx-react-devtools'
-
 import {inject, observer} from 'mobx-react'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import React, {Component} from 'react'
 
+import Animator from '../animator'
 import LobbyCard from './lobby-card'
 import LobbyList from './lobbies'
 import LobbySettings from './lobby-settings'
 import NavBar from './nav'
 import PrototypeList from './prototypes'
 
+const {timeouts} = Animator
+const LobbyCardTransition = props => <CSSTransition classNames="lobby-card" timeout={timeouts.lobbyCard} {...props}/>
+
 @inject('store') @observer export default class AppComponent extends Component {
+	page = React.createRef()
+	animator = new Animator(this.page)
+
 	render() {
+		const {
+			page,
+			animator,
+			props
+		} = this
+
 		const {
 			view: {showLobbySettings},
 			prototypes: {array: prototypes},
@@ -19,26 +31,60 @@ import PrototypeList from './prototypes'
 				expanded: expandedLobby,
 				hosting: hostingLobby
 			}
-		} = this.props.store
+		} = props.store
+
+		let expandedLobbyKey, hostingLobbyKey
+
+		if (expandedLobby) {
+			expandedLobbyKey = `lobby-card-${expandedLobby.id}`
+		}
+
+		if (hostingLobby) {
+			hostingLobbyKey = `lobby-card-${hostingLobby.id}`
+		}
 
 		return (
 			<main>
 				<NavBar/>
-				<div className="page clear-fix">
-					{!showLobbySettings && [
-						<PrototypeList key="prototypes" prototypes={prototypes}/>,
-						<LobbyList key="lobbies" lobbies={lobbies}/>
-					]}
+				<div ref={page} className="page clear-fix">
+					<TransitionGroup component={null}>
+						{!showLobbySettings &&
+							<CSSTransition
+								key="protos-lobbies"
+								classNames="protos-lobbies"
+								timeout={timeouts.protosLobbies}
+								onExit={animator.protosLobbiesExit}
+							>
+								<div className="protos-lobbies">
+									<PrototypeList key="prototypes" prototypes={prototypes}/>
+									<LobbyList key="lobbies" lobbies={lobbies}/>
+								</div>
+							</CSSTransition>
+						}
 
-					{expandedLobby &&
-						<LobbyCard key={`lobby-card-${expandedLobby.id}`} lobby={expandedLobby}/>
-					}
+						{expandedLobby &&
+							<LobbyCardTransition key={'lobby-card'} onEnter={animator.lobbyCardEnter} onExit={animator.lobbyCardExit}>
+								<TransitionGroup component={null}>
+									<LobbyCardTransition key={expandedLobbyKey} onExit={animator.lobbyCardSwitch}>
+										<LobbyCard key={expandedLobbyKey} lobby={expandedLobby}/>
+									</LobbyCardTransition>
+								</TransitionGroup>
+							</LobbyCardTransition>
+						}
 
-					{hostingLobby && showLobbySettings &&
-						<LobbySettings key={`lobby-settings-${hostingLobby.id}`} lobby={hostingLobby}/>
-					}
+						{hostingLobby && showLobbySettings &&
+							<CSSTransition
+								key={hostingLobbyKey}
+								classNames="lobby-settings"
+								timeout={timeouts.protosLobbies}
+								onEnter={animator.lobbySettingsEnter}
+								onExit={animator.lobbySettingsExit}
+							>
+								<LobbySettings key={hostingLobbyKey} lobby={hostingLobby}/>
+							</CSSTransition>
+						}
+					</TransitionGroup>
 				</div>
-				{/* <Dev/> */}
 			</main>
 		)
 	}
